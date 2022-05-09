6a5c6fb482c844751e0c0553fa00e867b218e019
WhiteHotaka <hakuhota0721@gmail.com>
2022-05-10 00:14:45 +0900

    カリキュラム8-6

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,11 +7,16 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     </head>
     <body>
-        
         <h1 class="title">
             {{ $post->title }}
         </h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+    @csrf
+    @method('DELETE')
+    <button type="submit">delete</button> 
+    　　<p class='delete'>[<span onclick="return deletePost(this);">delete</span>]</p>
+        </form>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
@@ -20,6 +25,14 @@
         </div>
         <div class="footer">
             <a href="/">戻る</a>
+    <script>
+        function deletePost(e) {
+           'use strict';
+           if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+               document.getElementById('form_delete').submit();
+           }
+        }
+    </script>        
         </div>
     </body>
 </html>
